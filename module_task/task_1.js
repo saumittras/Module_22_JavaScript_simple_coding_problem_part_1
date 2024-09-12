b72ccdfb4497ed0248ef41cc6c15227279c96935
saumittras <saumittra.eee@gmail.com>
2024-09-12 23:00:54 +0600

    added module task 1

---
+++ module_task/task_1.js
@@ -0,0 +1,9 @@
+//Task-1:
+//Write a function to convert temperature from Celsius to Fahrenheit.
+
+function celsiusToFahrenteit(celsius) {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+console.log(celsiusToFahrenteit(35));
